5760068acafa01bb2c477c1d7da6c3b3b2d2ce6a
Karolina Rasińska <rasinska.karolina@gmail.com>
2021-07-13 17:27:24 +0200

    add class active

--- js/script.js
+++ js/script.js
@@ -3,31 +3,34 @@
     console.log('links:', links);
   });*/
 
-  const titleClickHandler = function(event){
-    console.log('Link was clicked!');
+const titleClickHandler = function(event){
+const clickedElement = this;
+console.log('Link was clicked!');
   
-    /* remove class 'active' from all article links  */
+/* [DONE] remove class 'active' from all article links  */
 
-    const activeLinks = document.querySelectorAll('.titles a.active');
+  const activeLinks = document.querySelectorAll('.titles a.active');
 
-    for(let activeLink of activeLinks){
-    activeLink.classList.remove('active');
+  for(let activeLink of activeLinks){
+  activeLink.classList.remove('active');
 }
     
-    /* add class 'active' to the clicked link */
-  
-    /* remove class 'active' from all articles */
+  /* [IN PROGRESS] add class 'active' to the clicked link */
 
-    const activeArticles = document.querySelectorAll('.posts article.active');
-    for (let activeArticle of activeArticles) {
-    activeArticle.classList.remove('active');
+  clickedElement.classList.add('active');
+  
+  /* [DONE] remove class 'active' from all articles */
+
+  const activeArticles = document.querySelectorAll('.posts article.active');
+  for (let activeArticle of activeArticles) {
+  activeArticle.classList.remove('active');
   }
   
-    /* get 'href' attribute from the clicked link */
+  /* get 'href' attribute from the clicked link */
   
-    /* find the correct article using the selector (value of 'href' attribute) */
+  /* find the correct article using the selector (value of 'href' attribute) */
   
-    /* add class 'active' to the correct article */
+/* add class 'active' to the correct article */
   }
   
   const links = document.querySelectorAll('.titles a');
